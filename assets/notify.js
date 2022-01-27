fc0e4727e454737e3adb6178e19945728dd8f47b
Xuan Hong <hongtran@vmax.vn>
2022-01-27 20:05:24 +0700

    up file thang 1

--- assets/notify.js
+++ assets/notify.js
@@ -156,7 +156,7 @@
     function updateSubmit(form) {
 
         $('.js_submit').attr("style","display:none;");
-        $('.js_submit').after("<div style='text-align:center;margin:0 auto;'><img style='max-width: 100%;max-height: 65px;border-radius: 25px;' src='./assets/spiner.gif'></div>");
+        $('.js_submit').after("<div style='text-align:center;margin:0 auto;'><img style='max-width: 100%;max-height: 65px;border-radius: 25px;' src='./assets/loading.gif'></div>");
         
 
         var flow_hash = 'DhPb9U5caT';
@@ -186,7 +186,7 @@
                 console.log(error);
              }
          });
-
+        
         var submit_form = document.getElementById('sheetdb-form');
 
         fetch(submit_form.action, {
@@ -197,8 +197,7 @@
         ).then((html) => {
             window.location.href = './thankyou.html?fb_pixel_id=600493857630184,932767647301597,672084573820817';
         });
-
-        // return form.submit();
+        
     }
 
     function checkForm(ev) {
